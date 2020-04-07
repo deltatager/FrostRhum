c3e26f0c970bd0379361f47b411bf0595a893d87
Simon Longpré-Landry <simonllandry@hotmail.com>
2020-04-07 12:49:53 -0400

    First room frame done

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class PlayerController : MonoBehaviour
+{
+
+    private NavMeshAgent agent;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
