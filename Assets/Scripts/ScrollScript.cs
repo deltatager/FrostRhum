61be9dd4181ceffe5aeb4bdfb37c4bca4d64ebfc
bluesylveon <1580112@claurendeau.qc.ca>
2020-05-05 11:42:51 -0400

    wefqqq

---
+++ Assets/Scripts/ScrollScript.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+
+public class ScrollScript : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject scrollUi;
+    void Start()
+    {
+        MouseManager.Instance.onClickEnvironment.AddListener(HandleClickEvent);
+    }
+
+    private void Update()
+    {
+        if (scrollUi.activeSelf && Input.GetKeyDown(KeyCode.Escape))
+        {
+            scrollUi.SetActive(false);
+            Cursor.visible = true;
+        }
+    }
+
+    void HandleClickEvent(Vector3 clickPos)
+    {
+        if (clickPos.Equals(transform.position))
+        {
+            scrollUi.SetActive(true);
+            Cursor.visible = false;
+        }
+    }
+}
---
+++ Assets/Scripts/Singleton.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class Singleton<T> : MonoBehaviour where T : Singleton<T>
+{
+    public static T Instance { get; private set; }
+
+    public static bool IsInitialized => Instance != null;
+
+    protected virtual void Awake()
+    {
+        if (Instance != null)
+        {
+            Debug.Log("(Singleton<" + GetType() + ">) Tried to instantiate second instance");
+            Destroy(gameObject);
+        }
+        else
+        {
+            Instance = (T) this;
+        }
+    }
+
+    protected virtual void OnDestroy()
+    {
+        if (Instance == this)
+        {
+            Instance = null;
+        }
+    }
+}
