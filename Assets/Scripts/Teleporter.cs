42cf6ca98eed8f1b950180cc390df92fc79f2d68
Simon Longpré-Landry <simonllandry@hotmail.com>
2020-05-06 18:40:11 -0400

    i just commited cmon unity

---
+++ Assets/Scripts/Teleporter.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class Teleporter : MonoBehaviour
+{
+    [SerializeField] private Transform destination;
+    [SerializeField] private bool useDifferentCam;
+    [SerializeField] private Camera newCam;
+    
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!other.gameObject.CompareTag("Player")) return;
+
+        other.gameObject.GetComponent<NavMeshAgent>().Warp(destination.position);
+        if (!useDifferentCam) return;
+        
+        Camera.main.gameObject.SetActive(false);
+        newCam.gameObject.SetActive(true);
+    }
+}
