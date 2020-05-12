7830aeac56a90c173ad0c84210d5fa8782052fbd
Simon Longpré-Landry <simonllandry@hotmail.com>
2020-05-12 15:39:40 -0400

    whyuyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy

---
+++ Assets/Scripts/GameButton.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class GameButton : MonoBehaviour
+{
+    public ButtonClickedEvent buttonClickedEvent;
+    private Animation _anim;
+
+    private void Start()
+    {
+        _anim = GetComponent<Animation>();
+        MouseManager.Instance.onClickEnvironment.AddListener(OnClickedEvent);
+    }
+
+    private void OnClickedEvent(Vector3 point)
+    {
+        
+        if (!point.Equals(transform.position)) return;
+        _anim.Play();
+        buttonClickedEvent.Invoke();
+    }
+}
+
+[Serializable]
+public class ButtonClickedEvent : UnityEvent
+{
+}
