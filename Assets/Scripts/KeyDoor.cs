0763ae49a1288f5cfa6d37d0156770a08eff60dd
Simon Longpré-Landry <simonllandry@hotmail.com>
2020-05-19 22:30:10 -0400

    Done

---
+++ Assets/Scripts/KeyDoor.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyDoor : Door
+{
+
+    [SerializeField] private GameObject key;
+
+    public override void ToggleDoor()
+    {
+        if (!InventoryMenu.Instance.ContainsObject(key)) return;
+        InventoryMenu.Instance.UseObject(key);
+        base.ToggleDoor();
+    }
+}
